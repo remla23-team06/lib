1ba99fa1e0a4b08e432502cd2f9a43bf263b3cae
= <=>
2023-06-15 18:32:21 +0200

    Add test for remlaverlib code.

---
+++ tests/test_preprocess_data.py
@@ -0,0 +1,23 @@
+"""Tests for preprocess_data.py"""
+import re
+
+import pytest
+
+from src.remlaverlib.preprocessor import Preprocessor
+
+
+@pytest.fixture
+def preprocessor():
+    yield Preprocessor()
+
+
+def test_stopwords(preprocessor):
+    assert len(preprocessor.all_stopwords) > 0
+    assert "not" not in preprocessor.all_stopwords
+
+
+def test_process_input(preprocessor):
+    review = "The food was delicious!"
+    processed_review = preprocessor.process_input(review)
+    assert processed_review.islower() or processed_review == ""
+    assert not re.match("[^a-zA-Z]", processed_review)
