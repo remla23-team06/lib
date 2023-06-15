51840f24a346ef302415654736fe567432798ea5
= <=>
2023-06-15 20:14:55 +0200

    Add docstrings for class and method.

--- src/remlaverlib/version_util.py
+++ src/remlaverlib/version_util.py
@@ -2,8 +2,15 @@ from importlib import metadata
 
 
 class VersionUtil:
+    """
+    This class returns the current version of REMLA ver(sion) lib(rary)
+    """
     @classmethod
-    def get_version(cls):
+    def get_version(cls) -> str:
+        """
+        Returns the current version of remlaverlib
+        :return: a string with the version number like MAJOR.MINOR.PATCH
+        """
         try:
             return metadata.version('remlaverlib')
         except metadata.PackageNotFoundError:
