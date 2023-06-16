e31ec48a3a652956b6ea1c5fb9481ec3f8ee7e3d
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2023-06-16 17:41:33 +0200

    Add conftest.py for fixtures and remove tests from ignore in .pylintrc

--- tests/test_preprocess_data.py
+++ tests/test_preprocess_data.py
@@ -1,23 +1,23 @@
 """Tests for preprocess_data.py"""
 import re
 
-import pytest
-
-from src.remlaverlib.preprocessor import Preprocessor
-
-
-@pytest.fixture
-def preprocessor():
-    pre_proc = Preprocessor()
-    return pre_proc
-
 
 def test_stopwords(preprocessor):
+    """
+    Test if stopwords have been downloaded and "not" does not appear in stopwords
+    :param preprocessor:
+    :return: void
+    """
     assert len(preprocessor.all_stopwords) > 0
     assert "not" not in preprocessor.all_stopwords
 
 
 def test_process_input(preprocessor):
+    """
+    Test if input has been processed properly
+    :param preprocessor:
+    :return: void
+    """
     review = "The food was delicious!"
     processed_review = preprocessor.process_input(review)
     assert processed_review.islower() or processed_review == ""
