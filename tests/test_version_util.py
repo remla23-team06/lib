23e8120d2af8c971ed1b32c925f55d46ceb9da11
Rohan Sobha <r.r.sobha@student.tudelft.nl>
2023-06-16 18:00:40 +0200

    Add pydoc

--- tests/test_version_util.py
+++ tests/test_version_util.py
@@ -1,2 +1,7 @@
 def test_get_version_is_str(version_util):
+    """
+    Test if the get_version method returns a type of string
+    :param version_util:
+    :return: str containing a version
+    """
     assert isinstance(version_util.get_version(), str)
